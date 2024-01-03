b5334bfca5891dd06c23c590713ddc2ea81f09e5
rakeshjain1111 <rakeshkjain222@gmail.com>
2024-01-03 23:38:27 +0530

    first commit

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './Navbar.css'
+function Navbar() {
+  return (
+    <div className='nav'>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Gallery</li>
+            <li>Testimonial</li>
+            <li>Contact Us</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar;
