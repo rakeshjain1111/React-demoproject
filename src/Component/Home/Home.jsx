8075502a8ff6cd3c4bb5dc1a75106912802f50b5
rakeshjain1111 <rakeshkjain222@gmail.com>
2024-01-05 22:26:11 +0530

    final commit

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -7,6 +7,12 @@ function Home() {
         color:"#c0c0c0",              // <-- CSS AS const
         textAlign:"center"
      }
+
+     const cnc={title:"CNC Web World",
+                Subtitle:"Pune MH", 
+                Course1:"Java Full Stack",
+                Course2:"Python Full Stack"
+            }
      
    function myfun(){
     alert("This is test function")     // Define Function
@@ -15,7 +21,9 @@ function Home() {
   return (
      <div>    
          <h1 style={s}>Home</h1>
-         <button onClick={myfun} style={{width:"400px", height:"70px", color:"#fff",background:"#000",alignSelf:"center"}}>Click</button>      {/*  <-- Calling Function and Inline Css */}
+         <button onClick={myfun} style={{width:"400px", height:"70px",
+          color:"#fff",background:"#000",alignSelf:"center"}}>Click</button>      {/*  <-- Calling Function and Inline Css */}
+          <h1>{cnc.title}</h1>
      </div>
   );
 }
